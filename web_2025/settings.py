4b8ad9bd6df12c4ecae27de752dc13833ed72a7e
Podenko <podenko.ak@students.dvfu.ru>
2026-03-02 01:04:28 +1000

    подготовка к деплою 2

--- web_2025/settings.py
+++ web_2025/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', 'fefu_lab_db'),
         'USER': os.environ.get('DB_USER', 'fefu_user'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'СИЛЬНЫЙ_ПАРОЛЬ'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
